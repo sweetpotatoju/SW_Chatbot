49f96d8b3659f2dedc008829a8f2da407cec1414
jinhui-leee <fkdkfk1578@gmail.com>
2023-11-11 20:48:38 +0900

    chatbotAdmin app 추가

--- chatbotAdmin/views.py
+++ chatbotAdmin/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+# 비밀번호 페이지
+def admin_page(request):
+
+    if request.method == "POST":
+        password = request.POST.get('password')
+        if password == '1234':  # 관리자 비밀번호
+            # 비밀번호가 일치하면 메인 페이지로 리디렉션
+
+            return redirect('management')
+        else:
+            # 비밀번호가 틀렸을 때 별도의 응답 없이 아무 것도 하지 않음
+            return render(request, 'chatbotAdmin/admin_page.html')
+
+    return render(request, 'chatbotAdmin/admin_page.html')
