20fd6a183b5e44120abe4a3d90bcd6bfce92a138
jinhui-leee <fkdkfk1578@gmail.com>
2023-11-13 16:30:56 +0900

    import mysetting

--- SW_Chatbot/settings.py
+++ SW_Chatbot/settings.py
@@ -11,9 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-#mysetting 파일 찾지 못함
-#import mysetting
-#DATABASES= mysetting.DATABASES
+import mysetting
+DATABASES= mysetting.DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
