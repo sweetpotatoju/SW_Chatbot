96aad7cd1abc11025443a45a6d43ae74d6bf64a1
jinhui-leee <fkdkfk1578@gmail.com>
2023-11-13 14:40:23 +0900

    추가, 뒤로가기 버튼 팝업창 처리

--- chatbotAdmin/views.py
+++ chatbotAdmin/views.py
@@ -4,12 +4,10 @@ from django.shortcuts import render, redirect
 
 # 비밀번호 페이지
 def admin_page(request):
-
     if request.method == "POST":
         password = request.POST.get('password')
         if password == '1234':  # 관리자 비밀번호
             # 비밀번호가 일치하면 메인 페이지로 리디렉션
-
             return redirect('management')
         else:
             # 비밀번호가 틀렸을 때 별도의 응답 없이 아무 것도 하지 않음
