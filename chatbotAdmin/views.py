796ca5ceb845f51ea53763b66e691aacc575c8bd
guddnboy <hung3018@naver.com>
2023-12-04 01:07:34 +0900

    CSS 파일 분리

--- chatbotAdmin/views.py
+++ chatbotAdmin/views.py
@@ -81,12 +81,6 @@ def qatable_detail(request, pk):
     qatable = get_object_or_404(QATable, pk=pk)
     return render(request, 'chatbotAdmin/qatable_detail.html', {'qatable': qatable})
 
-# 유저 페이지에 공지 안내
-def user_page(request):
-    notices = Notice.objects.all()
-    return render(request, 'userpage/userpage.html', {'notices': notices})
-
-
 @csrf_exempt
 # 모델 훈련 업데이트를 수행하는 뷰
 def update_model(request):
--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@
 
 from django.urls import path
 from .views import chatbot_view
+from .views import notice_view
 
 urlpatterns = [
     path('', chatbot_view, name='chatbot_view'),
