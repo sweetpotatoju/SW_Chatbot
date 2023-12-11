b00e722e03ad9075dc1c280d2e536babcc17d896
jinhui-leee <fkdkfk1578@gmail.com>
2023-12-11 16:36:50 +0900

    html DB  action="{% url 'url이름' %}" 연결

--- SW_Chatbot/settings.py
+++ SW_Chatbot/settings.py
@@ -43,11 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'chatbotAdmin',
-    'corsheaders'
+    # 'corsheaders'
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,9 +57,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:8000',
+# ]
 
 ROOT_URLCONF = 'SW_Chatbot.urls'
 
